62501d53aea63107ee34429fc22a4ad28a53fef9
MKFeniks <44569532+MKFeniks@users.noreply.github.com>
2019-04-08 22:31:40 +0200

    Add files via upload

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stopwatch;
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exit = true;
+            while (exit)
+            {
+
+             Console.WriteLine("---------------------");
+             Console.WriteLine("Pres 1 to start stopwatch");
+             Console.WriteLine("Pres 2 to end stopwatch");
+             Console.WriteLine("Pres 3 to Exit");
+             Console.WriteLine("--------------------");
+                var pickOption = int.Parse(Console.ReadLine());
+               
+             Stopwatch timer = new Stopwatch();
+
+                if (pickOption == 1)
+                {
+
+
+                    timer.Start();
+
+                }
+                else if (pickOption == 2)
+                {
+                    timer.Stop();
+                    //timer.DurationOfTime();
+                }
+                else exit = false;
+         
+            }
+
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Stopwatch
+    {
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int Duration { get; set; }
+        
+        public string Start()
+        {
+            DateTime start = DateTime.Now;
+            StartTime = start.ToString("T");
+            Console.WriteLine($"Stopwatch started at {StartTime}");
+            return StartTime;
+        }
+        public string Stop()
+        {
+            DateTime stop = DateTime.Now;
+            EndTime = stop.ToString("T");
+            Console.WriteLine($"Stopwatch Stops at {EndTime}");
+            return EndTime;
+        }
+       
+        public void DurationOfTime()
+        {
+            Duration = int.Parse(EndTime) - int.Parse(StartTime);
+            Console.WriteLine($"Duration of the stopwatch is {Duration}" );
+            
+        }
+    }
+}
